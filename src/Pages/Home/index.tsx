25508b9664bf146e78d14b6b0acd4b2d23e1b9b9
Carlos Henrique <carlos_6282010@hotmail.com>
2020-08-27 16:59:34 -0300

    Iniciando com Axios

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ScrollView } from 'react-native';
 
@@ -10,19 +10,30 @@ import Modal from '../../components/Modal';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import api from '../../services/api';
+
 import { Container, SearchBar, Main, ModalText } from './styles';
 
 const Home: React.FC = () => {
+  const [posts, setPosts] = useState([]);
   const [modalIsVisible, setModalIsVisible] = useState(false);
 
   function toggleModal(): void {
     setModalIsVisible(!modalIsVisible);
   }
 
+  useEffect(() => {
+    api.get('posts').then(response => {
+      console.log(response);
+    }).catch(() => {
+      console.log('Erro ao consultar posts');
+    });
+  }, []);
+
   return (
     <Container>
       <SearchBar>
-        <Input name="email" icon="mail" placeholder="Buscar" />
+        <Input name="email" icon="mail" placeholder="Buscar"/>
       </SearchBar>
 
       <ScrollView>
